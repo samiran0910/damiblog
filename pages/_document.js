53c26a3db36a853e4820d97ec147396ce284e9ba
Shamiran Baishya <82767620+samiran0910@users.noreply.github.com>
2023-01-10 18:53:52 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,7 +1,6 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1005530345382977"
-     crossorigin="anonymous"></script>
+
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
